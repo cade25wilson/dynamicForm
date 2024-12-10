3a1b1afb23c0e215bcffb92d9a753376a5148863
Cade Wilson <cade25wilson@gmail.com>
2024-12-10 19:42:07 +0000

    Clear Git cache and reapply .gitignore

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,6 +12,13 @@
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <!-- Scripts -->
+        <script type="text/javascript">
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "paig81up2b");
+        </script>
         <!-- Google tag (gtag.js) -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-VM0K504M31"></script>
         <script>
